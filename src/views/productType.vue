744d93db64b44bf85fd72866085fb074bad5807c
guolin <15929675863@163.com>
2024-12-05 18:55:18 +0800

    no message

--- src/views/productType.vue
+++ src/views/productType.vue
@@ -87,7 +87,6 @@
           type: 1,
           platformIds: 3
         }
-        console.log(this.form,'form')
         lineProductList(queryParams).then(res => {
           if(res.rows && res.rows.length > 0) {
             // this.$router.push({
